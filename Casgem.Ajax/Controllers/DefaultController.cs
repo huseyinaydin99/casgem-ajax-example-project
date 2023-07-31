0d65360343aa6273fc95f8b22e53858d0916a557
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-07-31 10:50:12 +0300

    AJAX ile API'ye istek atıp tüm müşterileri Customers tablosundan çektik.

---
+++ Casgem.Ajax/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using Casgem.Ajax.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Casgem.Ajax.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly Context _context = new Context();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CustomerList()
+        {
+            var jsonCustomer = JsonConvert.SerializeObject(_context.Customers.ToList());
+            return Json(jsonCustomer);
+        }
+    }
+}
