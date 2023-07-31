809660b289c6d6182a7967ac45e98b5691bd5ddc
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-07-31 09:54:38 +0300

    Add project files.

---
+++ Casgem.Ajax/DAL/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace Casgem.Ajax.DAL
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string City { get; set; }
+    }
+}
